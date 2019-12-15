795162e8eb882a1e4fddfddce86b4a202c654611
Tymoteusz Czech <tymek@scrlk.pl>
2019-12-15 02:27:04 +0100

    feat: props spreading and arrow parens

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ module.exports = {
   ],
   rules: {
     semi: ['warn', 'never'],
+    'arrow-parens': 'as-needed',
     'unicorn/filename-case': 'off',
     'unicorn/prevent-abbreviations': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 }
